ee2fa17852a46a8ac5766c8393e4d56c5d92211d
elmatematic0 <wkdalebout+github@gmail.com>
2013-11-26 22:50:19 -0500

    Completing a form-based workflow feature.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  include ProductsHelper
+
   def index
     @products = Product.all
   end
@@ -8,8 +10,36 @@ class ProductsController < ApplicationController
   end 
 
   def new
+    @product = Product.new
   end
   
   def create
+    @product = Product.new(product_params)
+    @product.save
+    flash.notice = "Product '#{@product.name}' Created!"
+
+    redirect_to product_path(@product)
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+
+    flash.notice = "Product '#{@product.name}' Updated!"
+
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    flash.notice = "Product '#{@product.name}' Deleted!"
+
+    redirect_to products_path
   end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
 end
