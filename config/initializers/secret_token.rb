d168e062bfc30d311cf970c5eeb4a8093d95db6e
elmatematic0 <wkdalebout+github@gmail.com>
2013-11-26 21:34:12 -0500

    first forestrain commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forestrain::Application.config.secret_key_base = '66b25f629355b1666e7273c7374b33c9f7550ac798270b329eb10a9ace06836bbf73dd0fe08050b729d839d7c29649ce72083df8170f16aa46e8d2aefa46282c'
